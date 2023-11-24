3ec14408ae6ac3e4bcd6a06db4ebc6874ef35ba5
faisalnoufal <faisalnoufal@gmail.com>
2023-11-24 21:21:12 +0530

    revert events

--- src/routes/api/events/+server.ts
+++ src/routes/api/events/+server.ts
@@ -1,56 +1,34 @@
 import { json } from '@sveltejs/kit';
-import fs from 'fs/promises';
-import path from 'path';
-import matter from 'gray-matter';
+// import type {  } from '$lib/types';
 
 import type { Events } from '$lib/types';
 
 async function getEvents() {
 	let events: Events[] = [];
 
-	const eventsDirectory = 'src/data/events'; // Adjust the path based on your project structure
+	const paths = import.meta.glob('/src/data/events/*.md', { eager: true });
 
-	try {
-		const files = await fs.readdir(eventsDirectory);
+	for (const path in paths) {
+		const file = paths[path];
+		const slug = path.split('/').at(-1)?.replace('.md', '');
 
-		for (const file of files) {
-			const filePath = path.join(eventsDirectory, file);
-			console.log('Reading file:', filePath);
-
-			const content = await fs.readFile(filePath, 'utf-8');
-			console.log('File content:', content);
-
-			const { data } = matter(content);
-			console.log('Parsed frontmatter:', data);
-
-			const slug = file.replace('.md', '');
-			const event = { ...data, slug } as Events;
-
-			if (event.published) {
-				events.push(event);
-			}
+		if (file && typeof file === 'object' && 'metadata' in file && slug) {
+			const metadata = file.metadata as Omit<Events, 'slug'>;
+			const event = { ...metadata, slug } satisfies Events;
+			event.published && events.push(event);
 		}
-
-		events.sort(
-			(first, second) => new Date(second.date).getTime() - new Date(first.date).getTime()
-		);
-
-		console.log('Fetched events:', events);
-	} catch (error) {
-		console.error('Error reading events:', error.message);
-		throw error; // Propagate the error to notify the application
 	}
+
+	events = events.sort(
+		(first, second) => new Date(second.date).getTime() - new Date(first.date).getTime()
+	);
 
 	return events;
 }
 
 export async function GET() {
-	try {
-		const events = await getEvents();
-		return json({
-			events
-		});
-	} catch (error) {
-		return json({ error: error.message }, 500); // Return an error response
-	}
+	const events = await getEvents();
+	return json({
+		events
+	});
 }
